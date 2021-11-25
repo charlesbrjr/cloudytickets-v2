ae73e7983943170dda56a467e9d3bbcae660c1eb
Shayane Teixeira <shayaneteixeira56@gmail.com>
2021-11-25 19:03:14 -0300

    Update and rename buyticketshow.html to buyticketshow.php

--- src/buyticketshow.php
+++ src/buyticketshow.php
@@ -6,31 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <title>Document</title>
+    <title> <?php echo $_SESSION['nome']; ?></title>
 
     <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 
 <body>
-
-    <!-- Sidebar -->
-    <div class="w3-sidebar w3-bar-block" style="width:5%">
-        <h3 class="w3-bar-item">
-            <a href="home.html">
-                <img class="logo" src="../assets/images/ticket.svg" alt="logo">
-            </a>
-        </h3>
-        <a href="songs.html" class="w3-bar-item">
-            <img class="icons" src="../assets/images/songs.svg" alt="logo">
-        </a>
-        <a href="concerts.html" class="w3-bar-item">
-            <img class="icons" src="../assets/images/show.svg" alt="logo">
-        </a>
-        <a href="party.html" class="w3-bar-item">
-            <img class="icons" src="../assets/images/party.svg" alt="logo">
-        </a>
-    </div>
-
+    <?php include '../sidebar.php'; ?>
     <!-- Page Content -->
     <div style="margin-left:5%">
         <div class="w3-container">
@@ -48,7 +30,7 @@
                 </p>
                 <label>Quantidade de Ingressos</label>
                 <input type="number" name="qtdticket" min="0">
-                <a href="home.html">
+                <a href="home.php">
                     <button>Comprar Ingressos</button>
                 </a>
                 <address>
@@ -58,6 +40,7 @@
             <br />
         </div>
     </div>
+    <?php include '../footer.php'; ?>
 </body>
 
 </html>
